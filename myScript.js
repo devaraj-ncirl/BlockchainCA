627b4c2e1529f455f4525470021e7945c53677d1
devaraj-ncirl <78797715+devaraj-ncirl@users.noreply.github.com>
2021-04-16 21:08:10 +0100

    first commit

---
+++ myScript.js
@@ -0,0 +1,379 @@
+const Web3 = require('web3')
+var Tx = require('ethereumjs-tx').Transaction
+const web3 = new Web3('https://ropsten.infura.io/v3/68a2b49863a641cfb6798f9328ac3031')
+
+const senderaccount = '0x9F788BcE580aa1F79AC3c1FCb00de7e2af6b77D5' 
+
+const privateKey1 = Buffer.from('ec755edd22c031f920c2f2743965c47594fcd6fbe4216e947ee7b781bfcc9fc0', 'hex') 
+
+const contractAddress = '0xC04A2A94780B7c8A286dd8Ad22BEC2C75E6C5175'
+
+const contractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "recipient",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "distributeToken",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "_totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "tokenOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+const contract = new web3.eth.Contract(contractABI, contractAddress)
+
+const getTransactionCount = async(account) => {
+  return await web3.eth.getTransactionCount(account)
+}
+
+const sendTransaction = async(raw) => {
+  return await web3.eth.sendSignedTransaction(raw)
+}
+
+const transferFunds = async(account1, addressar, amount) => {
+
+  let txCount = await getTransactionCount(account1)
+
+  console.log("txCount returned: " + txCount)
+
+  const txObject = {
+    nonce:    web3.utils.toHex(txCount),
+    gasLimit: web3.utils.toHex(1000000), 
+    gasPrice: web3.utils.toHex(web3.utils.toWei('30', 'gwei')),
+    to: contractAddress,
+    data: contract.methods.distributeToken(addressar, amount).encodeABI()
+  }
+
+  const tx = new Tx(txObject, {chain:'ropsten', hardfork: 'petersburg'})
+
+  tx.sign(privateKey1)
+
+  const serializedTx = tx.serialize()
+  const raw = '0x' + serializedTx.toString('hex')
+
+  console.log("raw hex transaction: " + raw)
+
+  console.log("about to send transaction")
+
+  let minedTransaction = await sendTransaction(raw)
+  console.log("transaction hash returned: " + minedTransaction.transactionHash)
+
+  return `txHash is: ${minedTransaction.transactionHash}`
+}
+
+// async methods
+const getBalanceOf = async(account) => {
+  let balanceOf = await contract.methods.balanceOf(account).call()
+  return balanceOf
+}
+
+const fs = require('fs');
+const BigNumber = require('bignumber.js');
+
+const go = async() => {
+
+let distributionAddresses = fs.readFileSync("accounts.txt", 'utf8').split('\r\n');
+
+// get the balance of the token owner
+console.log(distributionAddresses)
+
+let ownerBalance = await getBalanceOf(senderaccount)
+console.log(`owner balance is ${ownerBalance}`)
+
+// get 5% of the owner's balance
+// convert balance to BigNumber
+let bal = new BigNumber(ownerBalance)
+
+let fivepercent = bal.div(20)
+
+
+console.log(fivepercent)
+console.log(`5% of owner balance is: ${fivepercent}`)
+
+
+// get the number of addresses in the file
+let numberOfAddresses = distributionAddresses.length
+
+console.log(`number of addresses in file: ${numberOfAddresses}`)
+
+// get the amount to distribute per address in the file
+//let distributionAmount = tokenBalanceSplit / numberOfAddresses
+let distributionAmount = fivepercent.div(numberOfAddresses)
+console.log(`amount to distribute per address: ${distributionAmount}`)
+
+  
+  await transferFunds(senderaccount, distributionAddresses, distributionAmount)
+}
+
+module.exports = { transferFunds, getBalanceOf }
+
+go()
+
+
